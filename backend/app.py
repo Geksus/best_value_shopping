ce13bad7162b47fdff96caaf7d817289822b8450
geksus <alexgergel@gmail.com>
2025-07-16 16:23:00 +0300

    add all items to db

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,66 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
-from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import json
+from datetime import datetime
+
+import os
+
+from models import Item
+from db import db
+
+load_dotenv()
+
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://username:password@localhost:3306/database_name'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{user}:{password}@{host}:3306/{db_name}'
 
-@app.route('/api/hello')
+db.init_app(app)
+
+@app.route('/')
 def hello():
-    data = requests.get('https://sf-ecom-api.silpo.ua/v1/uk/branches/1edb7347-7866-6c42-b1d6-11a6c487168c/products?limit=100&deliveryType=DeliveryHome&sortBy=popularity&sortDirection=desc&mustHavePromotion=false&inStock=true&offset=0')
+    print('App is running')
+    last_item = Item.query.order_by(Item.added_at.desc()).first()
+    if last_item is not None and last_item.added_at is not None and datetime.now().timestamp() - last_item.added_at.timestamp() < 86400:
+        return jsonify({'message': 'No new items'})
+
+    # Get all items from the database
+    data = requests.get('https://sf-ecom-api.silpo.ua/v1/uk/branches/1edb7347-7866-6c42-b1d6-11a6c487168c/products?limit=1&deliveryType=SelfPickup&sortBy=popularity&sortDirection=desc&mustHavePromotion=false&inStock=true&offset=0')
     json_data = data.json()
     total = json_data.get('total')
     items = []
-    for i in range(0, 20000, 100):
-        data = requests.get(f'https://sf-ecom-api.silpo.ua/v1/uk/branches/1edb7347-7866-6c42-b1d6-11a6c487168c/products?limit=100&deliveryType=DeliveryHome&sortBy=popularity&sortDirection=desc&mustHavePromotion=false&inStock=true&offset={i}')
+    for i in range(0, int(total), 100):
+        data = requests.get(f'https://sf-ecom-api.silpo.ua/v1/uk/branches/1edb7347-7866-6c42-b1d6-11a6c487168c/products?limit=100&deliveryType=SelfPickup&sortBy=popularity&sortDirection=desc&mustHavePromotion=false&inStock=true&offset={i}')
         json_data = data.json()
         items.extend(json_data.get('items'))
-    return items
+        print(len(items))
+
+    # Store items in the database
+    Item.query.delete()
+    db.session.commit()
+
+    print('Starting to add items')
+
+    for item in items:
+        item_id = item.get('id')
+        if item_id is not None:
+            item_fields = {col.name for col in db.Model.metadata.tables[Item.__tablename__].columns}
+        filtered_item = {k: item.get(k) if k not in ['promotions', 'specialPrices', 'modifier'] else json.dumps(item.get(k)) for k in item_fields}
+
+        existing = Item.query.get(filtered_item['id'])
+        if existing is not None:
+            print(f'Item {filtered_item["id"]} already exists')
+            continue
+        db.session.add(Item(**filtered_item))
+    db.session.commit()
+
+    return jsonify({'message': f'{len(items)} items processed and stored.'})
 
 if __name__ == '__main__':
     app.run(debug=True)
